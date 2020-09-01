040dabe7437f121cc2ae74368db6d93880d0050f
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-09-01 10:19:13 -0300

    first commit

---
+++ src/banco/config.php
@@ -0,0 +1,13 @@
+<?php
+
+    date_default_timezone_set('America/Sao_Paulo');
+
+    define ('DBHOST', 'localhost');
+    define ('DBUSER' , 'root');
+    define ('DBPASSWORD', '');
+    define ('DBNAME', 'db_appjuridico');
+    define ('MSG_FALHA_CONEXAO', 'Ops... não foi possivel conectar ao banco de dados!');
+    define ('MSG_CAMPOS_OBRIGATORIOS', 'Exite(m) campo(s) obrigatório(s) vazio(s)');
+    define ('TP_MSG_ERROR', 'error');
+    define ('TP_MSG_SUCCESS', 'success');
+    define ('TP_MSG_INFO', 'INFO');
